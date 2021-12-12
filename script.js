b6b237938d1c3f9b8375bf5740f735589dd6657c
skolotaja <94806629+skolotaja@users.noreply.github.com>
2021-12-13 00:14:43 +0200

    pedejais

--- script.js
+++ script.js
@@ -1,1 +1,112 @@
-alert(1)
+let rindas = document.querySelector('.rindas');
+let ciparuSkaits;
+let variantuSkaits=5;
+//let papildusVards;
+
+const vardi1 = ['spalvains', 'karsta', 'papedis', 'mazais', 'pinkains'];
+const vardi2 = ['lacis', 'vista', 'ruksis', 'sunis', 'sesks', 'kakis'];
+const vardi3 = ['lien', 'klukst', 'lec', 'lido', 'skrien', 'naud'];
+
+
+
+function rand(maxCipars) {
+    random1 = Math.floor(Math.random() * maxCipars);
+    return random1;
+}
+
+
+
+function veidotParoli() {
+    for(let i=0;i<variantuSkaits;i++){
+    
+        let random1 = rand(vardi1.length);
+        let random2 = rand(vardi2.length);
+        let random3 = rand(vardi2.length);
+
+        let parole1 = vardi1[random1];
+        let parole2 = vardi2[random2];
+        let parole3 = vardi3[random3];
+    }
+        //let parole = parole1 + parole2 + parole3;
+
+
+        //let paroleVards1 = tekstuSarezgit( vardi1[random1] );
+        //paroleVards1 = paroleVards1.replace('a','@');
+
+        //let paroleVards2;
+
+        //if( papildusVards != '' )
+        //{
+        //paroleVards2 = tekstuSarezgit( papildusVards );
+        //}
+        //else
+        //{
+        //paroleVards2 = tekstuSarezgit( vardi2[random2] );
+        //}
+
+        //let paroleVards3 = tekstuSarezgit( vardi3[random3] );
+
+        //let cipari = cipariParolei(ciparuSkaits);
+
+
+        //let parole = parole1+parole2+parole3+cipari;
+
+        //return parole;
+}
+
+
+
+
+        function cipariParolei(ciparuSkaits)
+        {
+        let cipars = String();
+
+        for (let i = 0; i < ciparuSkaits; i++) 
+        {
+        cipars += rand(9);
+        }
+        return cipars;
+        }
+        //function tekstuSarezgit(teksts)
+        //{
+        //if(rand(10) > 5)
+        //{
+        //teksts = teksts.replace('i','1');
+        //teksts = teksts.replace('a','@');
+        //}
+        //teksts = lieloPirmoBurtu(teksts);
+
+        //return teksts;
+        //}
+        //function lieloPirmoBurtu(vards)
+        //{
+        //let pirmaisBurts = vards.substr(0, 1);
+        //pirmaisBurts = pirmaisBurts.toUpperCase();
+        //let parejaisVards = vards.substr(1);
+
+        //return pirmaisBurts+parejaisVards;
+        //}
+
+
+
+        function generetParoles() {
+        ciparuSkaits = document.querySelector('#ciparuSkaits').value;
+        variantuSkaits = document.querySelector('#variantuSkaits').value;
+        //papildusVards = document.querySelector('#papildusVards').value;
+        rindas.innerHTML = '';
+
+        for (let i = 0; i < variantuSkaits; i++) {
+
+        //let parole=parole1+parole2+parole3+cipari;
+        //return parole;
+        let parole = veidotParoli(ciparuSkaits);
+
+        rindas.innerHTML += `
+    <tr>
+    <td>${i + 1}</td>
+    <td>${parole}</td>
+    </tr>`;
+
+    }//cikla beigas
+}//generetParoles beigas
+
